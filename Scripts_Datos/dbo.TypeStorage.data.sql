4161778bb618fb4553bf44a18e0d87777ca705a4
Hernando Delgado <hernando.delgado01@gmail.com>
2015-11-02 14:14:58 -0500

    Añadir archivos de script de base de datos

---
+++ Scripts_Datos/dbo.TypeStorage.data.sql
@@ -0,0 +1,9 @@
+SET IDENTITY_INSERT [dbo].[TypeStorage] ON
+INSERT INTO [dbo].[TypeStorage] ([TypeStorageId], [TypeStorageName]) VALUES (1, N'none')
+INSERT INTO [dbo].[TypeStorage] ([TypeStorageId], [TypeStorageName]) VALUES (2, N'Blu Ray')
+INSERT INTO [dbo].[TypeStorage] ([TypeStorageId], [TypeStorageName]) VALUES (3, N'Cd Rom')
+INSERT INTO [dbo].[TypeStorage] ([TypeStorageId], [TypeStorageName]) VALUES (4, N'External Hard Drive')
+INSERT INTO [dbo].[TypeStorage] ([TypeStorageId], [TypeStorageName]) VALUES (5, N'Hard Drive')
+INSERT INTO [dbo].[TypeStorage] ([TypeStorageId], [TypeStorageName]) VALUES (6, N'VHS')
+INSERT INTO [dbo].[TypeStorage] ([TypeStorageId], [TypeStorageName]) VALUES (7, N'USB Drive')
+SET IDENTITY_INSERT [dbo].[TypeStorage] OFF
---
+++ Scripts_Datos/dbo.User.data.sql
@@ -0,0 +1,13 @@
+SET IDENTITY_INSERT [dbo].[User] ON
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (1, N'MovieLector', N'legol2@hotmail.com', N'12345')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (2, N'UserMovies 56', N'abuchar05@gmail.com', N'123')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (3, N'Dev05', N'hernando.delgado01@gmail.com', N'846')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (4, N'nando05', N'abuchar05@yahoo.com', N'987')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (5, N'cinecine', N'legol2@hotmail.com', N'Fabric@50')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (6, N'cinefilia', N'andres76@hotmail.com', N'0847')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (7, N'walter', N'walter50@hotmail.com', N'1010')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (9, N'mariocine', N'mariocine@gmail.com', N'6060')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (10, N'cineCarlos', N'carlitos@hotmail.com', N'7070')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (11, N'hildacine', N'hildacine@gmail.com', N'5252')
+INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [Password]) VALUES (12, N'mariapelicula', N'mariapelicula@gmail.com', N'8989')
+SET IDENTITY_INSERT [dbo].[User] OFF
